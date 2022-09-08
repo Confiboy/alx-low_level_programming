60a075a7c306a5914173e3b32d89ccc2ee61bc4a
Confiboy <ehiemereconfidence23@gmail.com>
2022-09-08 14:55:54 -0700

    task 4

---
+++ 4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+   
+/**
+ * main - Reading
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like buiding a multilingual
+puzzle");
+	return (0);
+}
