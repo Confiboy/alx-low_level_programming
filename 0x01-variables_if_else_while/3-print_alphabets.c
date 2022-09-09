478060536f0e1bee6ce21812b2c52a7f7cdcbc57
Confiboy <ehiemereconfidence23@gmail.com>
2022-09-09 13:35:22 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char love;
+
+	for (love = 'a'; love <= 'z';
+		putchar(love);
+	for (love = 'A'; love <= 'Z';
+		putchar(love);
+	putchar('\n');
+	return (0);
+}
