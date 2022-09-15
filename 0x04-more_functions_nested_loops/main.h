d7fff99bb89b9f4334c3c9879c65c887fba28ce2
Confiboy <ehiemereconfidence23@gmail.com>
2022-09-15 12:21:40 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,13 @@
-int _isupper(int c);
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototypes
+ * void prototypes (void)
+ * int prototypes (int)
+ */
+
+int _isupper(int);
+
+#endif /* MAIN_H */
 
