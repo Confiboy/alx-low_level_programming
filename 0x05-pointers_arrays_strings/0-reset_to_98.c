0bcb3ef0d0b1d68fae2209e1688b8e98c485e595
Confiboy <ehiemereconfidence23@gmail.com>
2022-09-19 10:21:38 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset 0-reset_to_98.c -reset the value of the given integer to 98
+ * Qn: Parameter
+ * Return : nothing
+ */
+
+void reset_to_98(int *n);
+{
+	n=98;
+}
